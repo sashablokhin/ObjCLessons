2ca8232995784d84adea41bc4da47a137b6651d7
sashablokhin <sashablokhindev@gmail.com>
2016-05-16 16:36:53 +0300

    Update methods example

--- Methods/Methods/main.m
+++ Methods/Methods/main.m
@@ -2,15 +2,26 @@
 //  main.m
 //  Methods
 //
-//  Created by Alexander Blokhin on 12.05.16.
+//  Created by Alexander Blokhin on 16.05.16.
 //  Copyright © 2016 Alexander Blokhin. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+#import <Foundation/Foundation.h>
+#import "ABChildClass.h"
 
-int main(int argc, char * argv[]) {
+int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    
+        ABChildClass *childClass = [[ABChildClass alloc] init];
+        [childClass sayHello];
+        [childClass say:@"How are you?"];
+        [childClass say:@"I was worried about you" and:@"where have you been?"];
+        [childClass say:@"Hope you are ok!" and:@"Are you?" andAfterThat:@"Are you sure?"];
+        
+        NSLog(@"%@", [childClass saySomething]);
+        
+        [ABChildClass whoAreYou];
+        
     }
+    return 0;
 }
